01f6fa2d2ce44bf651e8fc6f9a1406a82c1ec4cd
arjun-saseendran <arjunsaseendran@icloud.com>
2025-02-01 06:38:33 +0530

    add varibles.rb

---
+++ day_02/variables.rb
@@ -0,0 +1,14 @@
+# Local variable
+  name = 'Ruby'
+
+# Golbal variable
+  $name = 'Ruby'
+
+# Class variable
+  @@name = 'Ruby'
+
+# Instance variable
+  @name = 'Ruby'
+
+# Constant variable
+  NAME =  'Ruby'
