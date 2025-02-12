0d3b255962fdc1207842fb684bcecbab045f910d
arjun-saseendran <arjunsaseendran@icloud.com>
2025-02-12 09:51:05 +0530

    add floats.rb

---
+++ floats.rb
@@ -0,0 +1,19 @@
+
+
+
+
+num = 10
+num1 = 3
+div = num / num1
+puts div
+# Float division
+div =  num.to_f / num1
+puts div
+num = 10.5
+puts num.class
+rud = num.round
+puts rud
+cel = num.ceil
+puts cel
+flo = num.floor
+puts flo
