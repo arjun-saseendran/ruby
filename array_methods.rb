28107c4bc81dbd0969a55f547d4968751e208363
arjun-saseendran <arjunsaseendran@icloud.com>
2025-02-14 21:30:02 +0530

    add array_methods.rb

---
+++ array_methods.rb
@@ -0,0 +1,42 @@
+
+
+
+arr = [1, 'a', [4], nil]
+print("The array is: ", arr)
+print("\nThe array length is: ",arr.length)
+arr.size
+print("\nArray reverse order is: ", arr.reverse)
+print("\nThe shuffle array is: ",arr.shuffle)
+arr<<1
+print("\nArray after adding new element to end: ", arr)
+print("\nArray after removing duplicate: ", arr.uniq)
+print("\nRemove empty position from array: ",arr.compact)
+print("\nFlatten array is: ", arr.flatten)
+print("\nSearch element 'a' in the array: ", arr.include?('a'))
+print("\nDelete element in a position: ",arr.delete_at(1))
+print("\nDelete a element: ", arr.delete(1))
+arr = [1, 2, 3, 4, 5]
+print("\nConvert array by joining to string: ", arr.join)
+print("\nJoin array with coma: ",arr.join(','))
+arr = "1, 2, 3, 4, 5"
+print("\nConvert string to array with split method: ", arr.split(','))
+arr = [1, 2, 3, 4, 5]
+print("\nFind first element of the array: ", arr.first)
+print("\nFind last element of the array: ", arr.last)
+print("\nAdd element to array: ",arr.push(6))
+print("\nUpdated array is: ", arr)
+print("\nRemove element from the array: ", arr.pop())
+print("\nUpdated array is: ", arr)
+print("\nRemove element from the start: ", arr.shift)
+print("\nUpdated array is: ", arr)
+print("\nAdd element through start: ", arr.unshift(6))
+print("\nUpdated array is: ", arr)
+arr1 = [6, 7, 8, 9, 0]
+print("\nAdd two arrays: ", arr + arr1)
+print("\nUpdated array is: ", arr)
+print("\nSutract array is: ", arr - arr1)
+print("\nUpdated array is: ", arr)
+
+
+
+
