7b53f784a3aea713a87cc51f04fd4ce3548629cf
arjun-saseendran <arjunsaseendran@icloud.com>
2025-02-15 10:03:07 +0530

    add booleans.rb

---
+++ booleans.rb
@@ -0,0 +1,17 @@
+
+
+
+num = 5
+puts('Is 5 grater than 6? ', num > 6)
+puts('Is 5 less than 6? ', num < 6)
+
+lang = {
+  'title' => 'Ruby',
+  'author' => 'Matz'
+
+}
+
+puts("Check the key of lang hash: ", lang.has_key?('title'))
+puts("Check the value fo lang hash: ", lang.has_value?('Matz'))
+
+
