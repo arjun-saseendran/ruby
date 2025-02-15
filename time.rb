e1d43ef18c9a3fa2cf82b0b39d37e15105607c02
arjun-saseendran <arjunsaseendran@icloud.com>
2025-02-15 21:59:59 +0530

    add time.rb

---
+++ time.rb
@@ -0,0 +1,16 @@
+
+
+time = Time.now
+
+print("The time is: ", time)
+puts
+print("The date is: ", time.day)
+puts 
+print("The month is: ", time.month)
+puts
+print("The year day is: ", time.yday)
+puts
+print("The week day is: ", time.wday)
+puts 
+print("The time zone is: ", time.zone)
+
