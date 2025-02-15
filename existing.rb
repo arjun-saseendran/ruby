cf4fd23a1e31de8c297da11833aedd919d4964ab
arjun-saseendran <arjunsaseendran@icloud.com>
2025-02-15 21:06:23 +0530

    add exiting

---
+++ existing.rb
@@ -0,0 +1,12 @@
+
+!#/usr/bin/env ruby
+
+1.upto(10) do |n|
+  if n == 5
+  # exit 
+    # exit!
+    abort("Exiting on file")
+  end
+
+  puts n
+end
---
+++ exiting.rb
@@ -0,0 +1,10 @@
+
+!#/usr/bin/env ruby
+
+1.upto(10) do |n|
+  if n == 5
+    exit
+  end
+
+  puts n
+end
