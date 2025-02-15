1582105e4acb2e63e0f46f67fd08d5db68d05843
arjun-saseendran <arjunsaseendran@icloud.com>
2025-02-15 09:02:04 +0530

    add hashes.rb

---
+++ hashes.rb
@@ -0,0 +1,17 @@
+
+
+lan = {
+ 'title' => 'Ruby',
+  'type' => 'Object oriented'
+}
+
+print('The language type is: ', lan['type'])
+lan['title'] = 'Python'
+print("\nUpdated hash list after updated title is: ", lan)
+lan['origin'] = 'Japan'
+print("\nUpdated hash list after added new element: ", lan)
+print("\nDisplay all keys in the hash list: ", lan.keys)
+print("\nDisplay all vales in the hash list: ", lan.values)
+print("\n Display length of the hash list: ", lan.length)
+print("\nDisplay size of the hash list: ", lan.size)
+
