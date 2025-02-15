c919f0261b423658a01740b27608bd13991f031d
arjun-saseendran <arjunsaseendran@icloud.com>
2025-02-15 09:27:40 +0530

    add symbols.rb

---
+++ symbols.rb
@@ -0,0 +1,18 @@
+
+
+
+# Hashes
+pg = {
+  :title => 'Javascript',
+  :author => 'Brendan Eich,'
+}
+
+print("\nThe programming language is: ", pg[:title])
+
+# Shorthand
+pg1 = {
+  title: 'Ruby',
+  author: 'Matz'
+}
+
+print("\nThe programming language is: ", pg1[:title])
