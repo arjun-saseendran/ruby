c90a2de71febd098a6486ea450d5de7379c2084e
arjun-saseendran <arjunsaseendran@icloud.com>
2025-02-15 18:46:49 +0530

    add case.rb

---
+++ case.rb
@@ -0,0 +1,25 @@
+
+num = 5
+
+case
+when num == 1
+  puts("The number is: 1")
+when num === 4
+  puts("The number is: 4")
+when num === 5
+  puts("The answer is correct, It is 5")
+else 
+  puts("You don't know that number")
+end
+
+
+case num
+when 1
+  puts('The number is 1')
+when 3
+  puts('The number is 3')
+when 5
+  puts('The answer is correct, It is 5')
+else
+  puts('Invalid input!')
+end
